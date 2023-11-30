b22b3be20e9ec51893b5653133132ae2ae755b16
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-11-30 15:52:06 +0200

    g

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//fixed top to header nav bar
+
+function setNavToTop() {
+  const navbar = document.querySelector("header");
+  const content = document.querySelector("#content");
+
+  if (window.scrollY > navbar.offsetHeight + 50) {
+    navbar.classList.add("fixed-top");
+    content.style.paddingTop = navbar.offsetHeight + "px";
+  } else {
+    navbar.classList.remove("fixed-top");
+    content.style.paddingTop = "0";
+  }
+}
+setNavToTop();
+
+window.onscroll = function () {
+  setNavToTop();
+};
