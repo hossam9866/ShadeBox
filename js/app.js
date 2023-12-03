6405408aecfa0118a3fd91bcee408c63c6daac05
hossam9866 <77072140+hossam9866@users.noreply.github.com>
2023-12-03 16:17:43 +0200

    h

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ function setNavToTop() {
   const content = document.querySelector("#content");
 
   if (window.scrollY > navbar.offsetHeight + 50) {
-    navbar.classList.add("fixed-top");
+    navbar.classList.add("fixed-top", "fixed-top-animation");
     content.style.paddingTop = navbar.offsetHeight + "px";
   } else {
-    navbar.classList.remove("fixed-top");
+    navbar.classList.remove("fixed-top", "fixed-top-animation");
     content.style.paddingTop = "0";
   }
 }
